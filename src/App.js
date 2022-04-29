c09120f8cf159453fe74d33ee0c98a248fe75c48
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-04-29 10:06:38 -0700

    Completed routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React from 'react';
+
+import { Route, Link, Switch } from 'react-router-dom';
+
+import Home from './components/Home';
+import PizzaForm from './components/PizzaForm';
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='pizza' id='order-pizza'>Order Pizza</Link>
+      </nav>
+      <Switch>
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='/pizza'>
+          <PizzaForm id='pizza-form' />
+        </Route>
+      </Switch>
     </>
   );
 };
+
 export default App;
---
+++ src/components/PizzaForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PizzaForm() {
+    return (
+        <form id='pizza-form'>
+
+        </form>
+    )
+}
